5e822444d5839a5917f66e1029d15b6f4231fa46
Jake Kara <jake@jakekara.com>
2018-02-22 11:00:12 -0500

    Added viz folder with 2016 version

---
+++ viz/integratedSchools/data.js
@@ -0,0 +1,513 @@
+var theJson = [
+  {
+    "School": "Connecticut IB Academy",
+    "Black_Latino_Pct": 53.65,
+    "Reduced_Isolation_Pct": 46.35,
+    "lat": "41.75284577900004,",
+    "lng": -72.60449611
+  },
+  {
+    "School": "Pathways Academy of Technology and Design",
+    "Black_Latino_Pct": 71.12,
+    "Reduced_Isolation_Pct": 28.88,
+    "lat": "41.74056938200005,",
+    "lng": -72.63770463
+  },
+  {
+    "School": "Hartford Magnet Trinity College Academy",
+    "Black_Latino_Pct": 72.92,
+    "Reduced_Isolation_Pct": 27.08,
+    "lat": "41.75125322700006,",
+    "lng": -72.68669494
+  },
+  {
+    "School": "Global Experience Magnet School",
+    "Black_Latino_Pct": 67.43,
+    "Reduced_Isolation_Pct": 32.57,
+    "lat": "41.86972863700004,",
+    "lng": -72.7352271
+  },
+  {
+    "School": "Classical Magnet School",
+    "Black_Latino_Pct": 76.32,
+    "Reduced_Isolation_Pct": 23.68,
+    "lat": "41.77334487200005,",
+    "lng": -72.70027147
+  },
+  {
+    "School": "Museum Academy",
+    "Black_Latino_Pct": 67.95,
+    "Reduced_Isolation_Pct": 32.05,
+    "lat": "41.851007192000054,",
+    "lng": -72.66673712
+  },
+  {
+    "School": "Reggio Magnet School of the Arts",
+    "Black_Latino_Pct": 62.11,
+    "Reduced_Isolation_Pct": 37.89,
+    "lat": "41.80167256800007,",
+    "lng": -72.8190072
+  },
+  {
+    "School": "Two Rivers Magnet Middle School",
+    "Black_Latino_Pct": 55.52,
+    "Reduced_Isolation_Pct": 44.48,
+    "lat": "41.76024109900004,",
+    "lng": -72.65997968
+  },
+  {
+    "School": "Discovery Academy",
+    "Black_Latino_Pct": 57.18,
+    "Reduced_Isolation_Pct": 42.82,
+    "lat": "41.73814668700004,",
+    "lng": -72.67549714
+  },
+  {
+    "School": "Public Safety Academy",
+    "Black_Latino_Pct": 71.08,
+    "Reduced_Isolation_Pct": 28.92,
+    "lat": "41.949482670000066,",
+    "lng": -72.60192537
+  },
+  {
+    "School": "Academy of Aerospace and Engineering",
+    "Black_Latino_Pct": 58.63,
+    "Reduced_Isolation_Pct": 41.37,
+    "lat": "41.75136059600004,",
+    "lng": -72.68412374
+  },
+  {
+    "School": "Two Rivers Magnet High School",
+    "Black_Latino_Pct": 75,
+    "Reduced_Isolation_Pct": 25,
+    "lat": "41.75498712900003,",
+    "lng": -72.66346746
+  },
+  {
+    "School": "University of Hartford Magnet School",
+    "Black_Latino_Pct": 69.52,
+    "Reduced_Isolation_Pct": 30.48,
+    "lat": "41.79337989800007,",
+    "lng": -72.71630599
+  },
+  {
+    "School": "Connecticut River Academy",
+    "Black_Latino_Pct": 67.82,
+    "Reduced_Isolation_Pct": 32.18,
+    "lat": "41.75004990200006,",
+    "lng": -72.64279689
+  },
+  {
+    "School": "Academy of Aerospace and Engineering Elementary",
+    "Black_Latino_Pct": 55.26,
+    "Reduced_Isolation_Pct": 44.74,
+    "lat": "41.643139388000066,",
+    "lng": -72.66441061
+  },
+  {
+    "School": "Medical Professions and Teacher Preparation Academy",
+    "Black_Latino_Pct": 73.4,
+    "Reduced_Isolation_Pct": 26.6,
+    "lat": "41.851007192000054,",
+    "lng": -72.66673712
+  },
+  {
+    "School": "International Magnet School for Global Citizenship",
+    "Black_Latino_Pct": 59.14,
+    "Reduced_Isolation_Pct": 40.86,
+    "lat": "41.80660243600005,",
+    "lng": -72.59549214
+  },
+  {
+    "School": "Wintonbury Early Childhood Magnet School",
+    "Black_Latino_Pct": 57.76,
+    "Reduced_Isolation_Pct": 42.24,
+    "lat": "41.837399066000046,",
+    "lng": -72.74678679
+  },
+  {
+    "School": "Dr. Ramon E. Betances Early Reading Lab School",
+    "Black_Latino_Pct": 69.89,
+    "Reduced_Isolation_Pct": 30.11,
+    "lat": "41.75934810500007,",
+    "lng": -72.67311565
+  },
+  {
+    "School": "Breakthrough II Elementary School",
+    "Black_Latino_Pct": 81.21,
+    "Reduced_Isolation_Pct": 18.79,
+    "lat": "41.80389722700005,",
+    "lng": -72.7041756
+  },
+  {
+    "School": "Breakthrough Magnet School",
+    "Black_Latino_Pct": 74.65,
+    "Reduced_Isolation_Pct": 25.35,
+    "lat": "41.74699910100003,",
+    "lng": -72.7066482
+  },
+  {
+    "School": "Capital Community College Magnet Academy",
+    "Black_Latino_Pct": 87.5,
+    "Reduced_Isolation_Pct": 12.5,
+    "lat": "41.768190048000065,",
+    "lng": -72.6730179
+  },
+  {
+    "School": "Capital Preparatory Magnet Academy",
+    "Black_Latino_Pct": 84.6,
+    "Reduced_Isolation_Pct": 15.4,
+    "lat": "41.77208081900005,",
+    "lng": -72.67580623
+  },
+  {
+    "School": "Environmental Sciences Magnet School at Mary M Hooker",
+    "Black_Latino_Pct": 67.37,
+    "Reduced_Isolation_Pct": 32.63,
+    "lat": "41.74221367400003,",
+    "lng": -72.70122902
+  },
+  {
+    "School": "Great Path Academy Middle College High School at MCC",
+    "Black_Latino_Pct": 68.28,
+    "Reduced_Isolation_Pct": 31.72,
+    "lat": "41.76444852100008,",
+    "lng": -72.55462892
+  },
+  {
+    "School": "Hartford Prekindergarten Magnet",
+    "Black_Latino_Pct": 57.34,
+    "Reduced_Isolation_Pct": 42.66,
+    "lat": "41.77614722100003,",
+    "lng": -72.68063083
+  },
+  {
+    "School": "Journalism/Media Magnet (Grades 9, 10, & 11)",
+    "Black_Latino_Pct": 88.32,
+    "Reduced_Isolation_Pct": 11.68,
+    "lat": "41.79759810000007,",
+    "lng": -72.67002638
+  },
+  {
+    "School": "Montessori Magnet School at Annie Fisher",
+    "Black_Latino_Pct": 57.35,
+    "Reduced_Isolation_Pct": 42.65,
+    "lat": "41.791478331000064,",
+    "lng": -72.70780279
+  },
+  {
+    "School": "Montessori Magnet at Moylan School",
+    "Black_Latino_Pct": 66.02,
+    "Reduced_Isolation_Pct": 33.98,
+    "lat": "41.74627325900008,",
+    "lng": -72.69846835
+  },
+  {
+    "School": "Noah Webster Micro-Society Magnet",
+    "Black_Latino_Pct": 74.7,
+    "Reduced_Isolation_Pct": 25.3,
+    "lat": "41.76888323700007,",
+    "lng": -72.71223792
+  },
+  {
+    "School": "R.J. Kinsella Magnet School of Performing Arts",
+    "Black_Latino_Pct": 72.09,
+    "Reduced_Isolation_Pct": 27.91,
+    "lat": "41.75505562400008,",
+    "lng": -72.66501743
+  },
+  {
+    "School": "Sports and Medical Sciences Academy",
+    "Black_Latino_Pct": 68.32,
+    "Reduced_Isolation_Pct": 31.68,
+    "lat": "41.75222001900005,",
+    "lng": -72.66033495
+  },
+  {
+    "School": "STEM Magnet School at Annie-Fisher",
+    "Black_Latino_Pct": 57.77,
+    "Reduced_Isolation_Pct": 42.23,
+    "lat": "41.791478331000064,",
+    "lng": -72.70780279
+  },
+  {
+    "School": "STEM Magnet at Dwight (formerly Betances STEM)",
+    "Black_Latino_Pct": 73.79,
+    "Reduced_Isolation_Pct": 26.21,
+    "lat": "41.73985956200005,",
+    "lng": -72.67188386
+  },
+  {
+    "School": "University High School of Science and Engineering",
+    "Black_Latino_Pct": 58.33,
+    "Reduced_Isolation_Pct": 41.67,
+    "lat": "41.79215044700004,",
+    "lng": -72.70920621
+  },
+  {
+    "School": "Ana Grace Academy of the Arts Elementary School",
+    "Black_Latino_Pct": 68.36,
+    "Reduced_Isolation_Pct": 31.64,
+    "lat": "41.804857921000064,",
+    "lng": -72.83534284
+  },
+  {
+    "School": "Greater Hartford Academy of  the Arts High School - Full Time",
+    "Black_Latino_Pct": 63.39,
+    "Reduced_Isolation_Pct": 36.61,
+    "lat": "41.75136059600004,",
+    "lng": -72.68412374
+  },
+  {
+    "School": "GHAA [Half-Day]",
+    "Black_Latino_Pct": 28.81,
+    "Reduced_Isolation_Pct": 71.19,
+    "lat": "41.75136059600004,",
+    "lng": -72.68412374
+  },
+  {
+    "School": "Greater Hartford Academy of the Arts Magnet Middle",
+    "Black_Latino_Pct": 71.04,
+    "Reduced_Isolation_Pct": 28.96,
+    "lat": "41.755486260000055,",
+    "lng": -72.6640313
+  },
+  {
+    "School": "GHAMAS [Half-Day]",
+    "Black_Latino_Pct": 17.98,
+    "Reduced_Isolation_Pct": 82.02,
+    "lat": "41.755486260000055,",
+    "lng": -72.6640313
+  },
+  {
+    "School": "Glastonbury/East Hartford Magnet School",
+    "Black_Latino_Pct": 51.22,
+    "Reduced_Isolation_Pct": 48.78,
+    "lat": "41.70795057400005,",
+    "lng": -72.59242481
+  },
+  {
+    "School": "Metropolitan Learning Center for Global and International Studies",
+    "Black_Latino_Pct": 72.89,
+    "Reduced_Isolation_Pct": 27.11,
+    "lat": "41.87140057200003,",
+    "lng": -72.72465284
+  },
+  {
+    "School": "Montessori Magnet School (CREC)",
+    "Black_Latino_Pct": 66.19,
+    "Reduced_Isolation_Pct": 33.81,
+    "lat": "41.74952279200005,",
+    "lng": -72.68708775
+  },
+  {
+    "School": "Goodwin College Early Childhood Interdistrict Magnet School",
+    "Black_Latino_Pct": 70,
+    "Reduced_Isolation_Pct": 30,
+    "lat": "41.74505404900003,",
+    "lng": -72.63953009
+  },
+  {
+    "School": "Achievement First Hartford Academy Elementary School",
+    "Black_Latino_Pct": -1,
+    "Reduced_Isolation_Pct": -1,
+    "lat": 41.78415,
+    "lng": -72.697224
+  },
+  {
+    "School": "Achievement First Hartford Academy Middle School",
+    "Black_Latino_Pct": -1,
+    "Reduced_Isolation_Pct": -1,
+    "lat": 41.78415,
+    "lng": -72.697224
+  },
+  {
+    "School": "Achievement First Hartford High Academy",
+    "Black_Latino_Pct": -1,
+    "Reduced_Isolation_Pct": -1,
+    "lat": 41.78415,
+    "lng": -72.697224
+  },
+  {
+    "School": "Achievement First Summit Middle School",
+    "Black_Latino_Pct": -1,
+    "Reduced_Isolation_Pct": -1,
+    "lat": 41.774444,
+    "lng": -72.681602
+  },
+  {
+    "School": "Alfred E. Burr Elementary School",
+    "Black_Latino_Pct": -1,
+    "Reduced_Isolation_Pct": -1,
+    "lat": 41.7448076,
+    "lng": 41.7448076
+  },
+  {
+    "School": "Asian Studies Academy at Bellizzi School",
+    "Black_Latino_Pct": -1,
+    "Reduced_Isolation_Pct": -1,
+    "lat": 41.7400949,
+    "lng": -72.6719264
+  },
+  {
+    "School": "Burns Latino Studies Academy",
+    "Black_Latino_Pct": -1,
+    "Reduced_Isolation_Pct": -1,
+    "lat": 41.7616032,
+    "lng": -72.6904571
+  },
+  {
+    "School": "Culinary Arts Academy",
+    "Black_Latino_Pct": -1,
+    "Reduced_Isolation_Pct": -1,
+    "lat": 41.7998407,
+    "lng": -72.7065095
+  },
+  {
+    "School": "Dr. Frank T. Simpson-Waverly",
+    "Black_Latino_Pct": -1,
+    "Reduced_Isolation_Pct": -1,
+    "lat": 41.7949208,
+    "lng": -72.6816782
+  },
+  {
+    "School": "Dr. Michael D. Fox School",
+    "Black_Latino_Pct": -1,
+    "Reduced_Isolation_Pct": -1,
+    "lat": 41.742712,
+    "lng": -72.6658685
+  },
+  {
+    "School": "E. B. Kennelly School",
+    "Black_Latino_Pct": -1,
+    "Reduced_Isolation_Pct": -1,
+    "lat": 41.7349323,
+    "lng": -72.6940218
+  },
+  {
+    "School": "Sarah J. Rawson Elementary School",
+    "Black_Latino_Pct": -1,
+    "Reduced_Isolation_Pct": -1,
+    "lat": 41.7957218,
+    "lng": -72.7004645
+  },
+  {
+    "School": "John C. Clark, Jr. Elementary & Middle School",
+    "Black_Latino_Pct": -1,
+    "Reduced_Isolation_Pct": -1,
+    "lat": 41.7870529,
+    "lng": -72.677399
+  },
+  {
+    "School": "L.W. Batchelder Elementary",
+    "Black_Latino_Pct": -1,
+    "Reduced_Isolation_Pct": -1,
+    "lat": 41.733048,
+    "lng": -72.7075522
+  },
+  {
+    "School": "María C. Colón Sánchez Elementary",
+    "Black_Latino_Pct": -1,
+    "Reduced_Isolation_Pct": -1,
+    "lat": 41.7588094,
+    "lng": -72.6892546
+  },
+  {
+    "School": "Martin Luther King Jr. Elementary School",
+    "Black_Latino_Pct": -1,
+    "Reduced_Isolation_Pct": -1,
+    "lat": 41.7867851,
+    "lng": -72.6958813
+  },
+  {
+    "School": "McDonough Expeditionary Learning School (MELS)",
+    "Black_Latino_Pct": -1,
+    "Reduced_Isolation_Pct": -1,
+    "lat": 41.7491673,
+    "lng": -72.6980208
+  },
+  {
+    "School": "The Middle Grades Academy at West Middle",
+    "Black_Latino_Pct": -1,
+    "Reduced_Isolation_Pct": -1,
+    "lat": 41.7709888,
+    "lng": -72.6961921
+  },
+  {
+    "School": "OPPortunity High School",
+    "Black_Latino_Pct": -1,
+    "Reduced_Isolation_Pct": -1,
+    "lat": 41.7702751,
+    "lng": -72.693918
+  },
+  {
+    "School": "Parkville Community School",
+    "Black_Latino_Pct": -1,
+    "Reduced_Isolation_Pct": -1,
+    "lat": 41.7568965,
+    "lng": -72.707981
+  },
+  {
+    "School": "Renzulli Academy",
+    "Black_Latino_Pct": -1,
+    "Reduced_Isolation_Pct": -1,
+    "lat": 41.7914902,
+    "lng": -72.7016563
+  },
+  {
+    "School": "High School, Inc.",
+    "Black_Latino_Pct": -1,
+    "Reduced_Isolation_Pct": -1,
+    "lat": 41.7674214,
+    "lng": -72.678299
+  },
+  {
+    "School": "High School, Inc. (9th Grade)",
+    "Black_Latino_Pct": -1,
+    "Reduced_Isolation_Pct": -1,
+    "lat": 41.7674214,
+    "lng": -72.678299
+  },
+  {
+    "School": "HPHS Academy of Engineering and Green Technology",
+    "Black_Latino_Pct": -1,
+    "Reduced_Isolation_Pct": -1,
+    "lat": 41.7640799,
+    "lng": -72.700983
+  },
+  {
+    "School": "HPHS Law and Government Academy",
+    "Black_Latino_Pct": -1,
+    "Reduced_Isolation_Pct": -1,
+    "lat": 41.7640799,
+    "lng": -72.700983
+  },
+  {
+    "School": "HPHS Nursing Academy",
+    "Black_Latino_Pct": -1,
+    "Reduced_Isolation_Pct": -1,
+    "lat": 41.7640799,
+    "lng": -72.700983
+  },
+  {
+    "School": "IB Global Communications Academy",
+    "Black_Latino_Pct": -1,
+    "Reduced_Isolation_Pct": -1,
+    "lat": 41.7845969,
+    "lng": -72.6977256
+  },
+  {
+    "School": "Dr. James H Naylor/CCSU Leadership Academy",
+    "Black_Latino_Pct": -1,
+    "Reduced_Isolation_Pct": -1,
+    "lat": 41.7308733,
+    "lng": -72.6742693
+  },
+  {
+    "School": "Bulkeley High School Lower School",
+    "Black_Latino_Pct": -1,
+    "Reduced_Isolation_Pct": -1,
+    "lat": 41.7468553,
+    "lng": -72.6714903
+  }
+];
